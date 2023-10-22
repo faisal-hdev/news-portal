dd17cfe521fd08330ec9c13e4706d4bed7923b6a
faisal-hdev <itsfaisalchowdhury@gmail.com>
2023-10-22 19:16:00 +0600

    Spin loader feature are addedd

--- script.js/portal.js
+++ script.js/portal.js
@@ -1,20 +1,20 @@
-const allCategoryLoad = () => {
+const allCategoryLoad = () => {   
     const url = `https://openapi.programming-hero.com/api/news/categories`
     fetch(url)
         .then(res => res.json())
         .then(data => displayCategory(data.data.news_category))
-};
+    };
 
 const displayCategory = (categories) => {
-    categories.forEach(category => { 
+        categories.forEach(category => { 
         const categoryContainer = document.getElementById('category-container')
         const categoryDiv = document.createElement('div')
         // start spinner loader and dynamically data show in onclick
         categoryDiv.innerHTML = `
-            <a onclick="categoryDataLoad('${category?.category_id}'); toggleSpinner(true)" class="text-lg text-gray-600" href="#">${category.category_name}</a>
+            <a onclick="categoryDataLoad('${category?.category_id}'); toggleSpinner('${true}')" class="text-lg text-gray-600" href="#">${category.category_name}</a>
         `;
         categoryContainer.appendChild(categoryDiv);
-    });
+        });
 };
 allCategoryLoad();
 
@@ -23,33 +23,42 @@ const categoryDataLoad = (id) => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayCategoryData(data.data))
+        toggleSpinner(true);
 };
 
 const displayCategoryData = (categoryData) => {
     // Display 10 data only
-    categoryData = categoryData.slice(0, 10);
+    const categoryContainer = document.getElementById('card-container');
+    categoryContainer.textContent = '';
+    // Display no data found
+    const noFoundMsgEl = document.getElementById('no-found-msg');
+    if (categoryData.length === 0) {
+        noFoundMsgEl.classList.remove('hidden')
+    } else {
+        noFoundMsgEl.classList.add('hidden')
+    };
+    // Display all data 
     categoryData.forEach(singleData => {
-        const categoryContainer = document.getElementById('card-container')
         const carDiv = document.createElement('div')
         carDiv.innerHTML = `
             <div class="card lg:card-side bg-base-100 shadow-xl mt-16">
-                <figure><img class="p-4" src="${singleData.thumbnail_url}" alt="Album"/></figure>
+                <figure><img class="p-4" src="${singleData?.thumbnail_url}" alt="Album"/></figure>
                 <div class="card-body ">
-                <h2 class="card-title">Title ${singleData.title}</h2>
-                <p class='max-w-[850px]'> Details : ${singleData.details ? singleData.details : 'Not found'}</p>
+                <h2 class="card-title">Title ${singleData?.title}</h2>
+                <p class='max-w-[850px]'> Details : ${singleData?.details ? singleData?.details : 'Not found'}</p>
                 <div class="card-actions justify-between items-center flex mt-4">
                 <div class="flex">
-                <img class="mask mask-squircle w-12" src="${singleData.author.img}" />
+                <img class="mask mask-squircle w-12" src="${singleData?.author?.img}" />
                 <div class="ml-5">
-                <p>${singleData.author.name ? singleData.author.name : "N/A"}</p>
-                <p>${singleData.author.published_date ? singleData.author.published_date : "N/A"}</p>
+                <p>${singleData?.author?.name ? singleData?.author?.name : "N/A"}</p>
+                <p>${singleData?.author?.published_date ? singleData?.author?.published_date : "N/A"}</p>
                 </div>
                 </div>
                 <div class="ml-5">
-                <p>Total Views : ${singleData.total_view ? singleData.total_view : "N/A"}</p>
+                <p>Total Views : ${singleData?.total_view ? singleData?.total_view : "N/A"}</p>
                 </div>
                 <div class="ml-5">
-                <button class="btn rounded-full" onclick="my_modal_2.showModal();loadCategoryDetails('${singleData._id}')">Show Details</button>
+                <button class="btn rounded-full" onclick="my_modal_2.showModal();loadCategoryDetails('${singleData?._id}')">Show Details</button>
                 </div>
                 </div>
             </div>
@@ -58,7 +67,6 @@ const displayCategoryData = (categoryData) => {
     })
     // Stop spinner loader 
     toggleSpinner(false);
-    
 };
 
 const loadCategoryDetails = (news_id) => {
@@ -72,12 +80,12 @@ const displayCategoryDetails = (categoryDetailData) => {
     const modalContainer = document.getElementById("my_modal_2");
     modalContainer.innerHTML = `
         <div class="modal-box">
-            <h3 class="font-bold inline text-lg">Title : ${categoryDetailData.title ? categoryDetailData.title : 'N/A'}</h3>
+            <h3 class="font-bold inline text-lg">Title : ${categoryDetailData?.title ? categoryDetailData?.title : 'N/A'}</h3>
             <div class="mt-4">
-            <h4 class="mb-2">Category ID : ${categoryDetailData.category_id ? categoryDetailData.category_id : 'N/A'}</h4>
-            <h4 class="mb-2">Total Views : ${categoryDetailData.total_view ? categoryDetailData.total_view : 'N/A'}</h4>
-            <h4 class="mb-2">Author Name : ${categoryDetailData.author.name ? categoryDetailData.author.name : 'N/A'}</h4>
-            <h4 class="mb-2">Author Name : ${categoryDetailData.author.published_date ? categoryDetailData.author.published_date : 'N/A'}</h4>
+            <h4 class="mb-2">Category ID : ${categoryDetailData?.category_id ? categoryDetailData?.category_id : 'N/A'}</h4>
+            <h4 class="mb-2">Total Views : ${categoryDetailData?.total_view ? categoryDetailData?.total_view : 'N/A'}</h4>
+            <h4 class="mb-2">Author Name : ${categoryDetailData?.author?.name ? categoryDetailData?.author?.name : 'N/A'}</h4>
+            <h4 class="mb-2">Author Name : ${categoryDetailData?.author?.published_date ? categoryDetailData?.author?.published_date : 'N/A'}</h4>
             </div>
             </div>
             <form method="dialog" class="modal-backdrop">
@@ -95,8 +103,6 @@ const toggleSpinner = (isLading) => {
     } else {
         spinLoaderEl.classList.add('hidden')
     }
-}
+};
 
-
-
-categoryDataLoad();
+categoryDataLoad('01');
